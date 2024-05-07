31a91d55e0a74572d8327d2e204c2d763329efdb
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-05-07 17:10:35 +0200

    Fixing

--- D2.js
+++ D2.js
@@ -21,25 +21,35 @@ console.log("Il numero più grande è: " + piuGrande);
 */
 const x = 4;
 
-function notEqual(x) {
-  if (x !== 5) {
-    console.log("not equal");
-  }
+if (x !== 5) {
+  console.log("not equal");
 }
-
-notEqual(x);
 
 /* ESERCIZIO 3
   Scrivi un algoritmo che mostri "divisibile per 5" in console se un numero fornito è perfettamente divisibile per 5 (suggerimento: usa l'operatore modulo)
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const divisibile = 55;
+
+let risultato = divisibile % 5;
+console.log("Divisibile per 5 dato che risulta :" + risultato);
 
 /* ESERCIZIO 4
   Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const numero1 = 5;
+const numero2 = 3;
+
+if (numero1 === 8 || numero2 === 8) {
+  console.log("Uno dei due numeri e 8.");
+} else if (numero1 + numero2 === 8) {
+  console.log("La somma dei numeri e uguale a 8.");
+} else if (numero1 - numero2 === 8 || numero2 - numero1 === 8) {
+  console.log("La differenza tra i numeri e uguale a 8.");
+} else {
+  console.log("Nessuna delle condizioni e soddisfatta.");
+}
 
 /* ESERCIZIO 5
   Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
@@ -47,7 +57,10 @@ notEqual(x);
   Crea un algoritmo che determini l'ammontare totale che deve essere addebitato all'utente per il checkout.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const totalShoppingCart = 60;
+const costoSpedizione = totalShoppingCart > 50 ? 0 : 10;
+const costoTotale = totalShoppingCart + costoSpedizione;
+console.log("Costo totale: $" + costoTotale);
 
 /* ESERCIZIO 6
   Stai lavorando su un sito di e-commerce. Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
@@ -62,7 +75,28 @@ notEqual(x);
   Alla fine mostra il risultato in console.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let a = 1;
+let b = 2;
+let c = 3;
+if (a >= b && a >= c) {
+  if (b >= c) {
+    console.log(a, b, c);
+  } else {
+    console.log(a, c, b);
+  }
+} else if (b >= a && b >= c) {
+  if (a >= c) {
+    console.log(b, a, c);
+  } else {
+    console.log(b, c, a);
+  }
+} else {
+  if (a >= b) {
+    console.log(c, a, b);
+  } else {
+    console.log(c, b, a);
+  }
+}
 
 /* ESERCIZIO 8
   Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
@@ -74,19 +108,24 @@ notEqual(x);
   Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const numero = 12;
+if (numero % 2 === 0) {
+  console.log("Il numero fornito è pari.");
+} else {
+  console.log("Il numero fornito è dispari.");
+}
 
 /* ESERCIZIO 10
-  Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
-  let val = 7
-  if (val < 10) {
-      console.log("Meno di 10");
-    } else if (val < 5) {
-      console.log("Meno di 5");
-    } else {
-      console.log("Uguale a 10 o maggiore");
-    }
-*/
+  Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza. */
+
+let val = 7;
+if (val < 5) {
+  console.log("Meno di 5");
+} else if (val < 10) {
+  console.log("Meno di 10");
+} else {
+  console.log("Uguale a 10 o maggiore");
+}
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
@@ -100,28 +139,38 @@ const me = {
   skills: ["javascript", "html", "css"],
 };
 
+me.city = "Toronto";
+console.log(me);
+
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 12
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere la proprietà "lastName".
 */
-
-/* SCRIVI QUI LA TUA RISPOSTA */
+delete me.lastName;
+console.log(me);
 
 /* ESERCIZIO 13
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere l'ultimo elemento della proprietà "skills".
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+me.skills.pop();
+console.log(me);
 
 /* ESERCIZIO 14
   Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+numbers[0];
+console.log("prima del push", numbers);
+
+numbers.push(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log("dopo il push", numbers);
 
 /* ESERCIZIO 15
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+numbers.pop();
+numbers.push(100);
+console.log("aggiunta 100", numbers);
